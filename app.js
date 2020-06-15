d730a8e71e1148ac5be0945f20132975347548a3
Warecuber <warecuber@gmail.com>
2020-06-15 16:34:56 -0500

    Added authentication and credential cache

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var APIController = (() => {
-  let myURL = window.location.hash;
-  let URLSplit = myURL.split("&");
-  let token = URLSplit[0].replace("#access_token=", "");
+  var oauthToken = localStorage.getItem("oAuth");
 
   function loadMyInfo() {
     $.ajax({
       url: "https://api.spotify.com/v1/me",
       method: "GET",
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${oauthToken}`,
       },
       success: function (data) {
         UIController.addMe(data);
+      },
+      error: function (req, err) {
+        if (req.status === 400 || req.status === 401) {
+          UIController.loginPage();
+        } else console.log(err);
       },
     });
   }
@@ -21,17 +24,32 @@ var APIController = (() => {
       url: "https://api.spotify.com/v1/me/top/tracks",
       method: "GET",
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${oauthToken}`,
       },
       success: function (data) {
-        console.log(data);
+        UIController.listSongs(data.items);
       },
-      error: function (req) {
-        if (req.status === 400) {
-          UICtl.loginPage();
+      error: function (req, err) {
+        if (req.status === 400 || req.status === 401) {
+          UIController.loginPage();
+        } else {
+          console.log(err);
         }
       },
     });
+  }
+
+  function generateAPIToken() {
+    let myURL = window.location.hash;
+    let URLSplit = myURL.split("&");
+    token = URLSplit[0].replace("#access_token=", "");
+    history.pushState(
+      { urlPath: window.location.pathname },
+      "",
+      window.location.origin
+    );
+    localStorage.setItem("oAuth", token);
+    localStorage.setItem("loggedIn", true);
   }
 
   return {
@@ -40,6 +58,9 @@ var APIController = (() => {
     },
     mySongs: () => {
       loadMySongs();
+    },
+    genOauth: () => {
+      generateAPIToken();
     },
   };
 })(UIController);
@@ -55,11 +76,38 @@ var UIController = (() => {
 
   function addMyInfo(info) {
     $(".myInfo").html(`<span class='displayName'>${info.display_name}</span>`);
-    $('#LoginButton').html(`Logged in as ${info.email}`);
-    document.getElementById('LoginButton').classList.remove('button');
-    document.getElementById('LoginButton').classList.remove('loginButton');
-    document.getElementById('LoginButton').classList.add('loggedInInfo');
+    $("#LoginButton").html(`Logged in as ${info.email}`);
+    document.getElementById("LoginButton").classList.remove("button");
+    document.getElementById("LoginButton").classList.remove("loginButton");
+    document.getElementById("LoginButton").classList.add("loggedInInfo");
     console.log(info);
+  }
+
+  function listMySongs(list) {
+    list.forEach((element) => {
+      addASong(element);
+    });
+    console.log(list);
+  }
+
+  function addASong(song) {
+    let container = document.querySelector(".mySongs");
+    let newSong = document.createElement("div");
+    newSong.classList.add("songElement");
+    newSong.innerHTML = `<span class='songName'>${song.name}</span><span class'songArtist'> - ${song.artists[0].name}</span><br><br><div class='songMedia'><audio controls class='songPreview'><source src='${song.preview_url}'></audio><img src='${song.album.images[0].url}' class='songImg'></div>`;
+    container.insertAdjacentElement("beforeend", newSong);
+  }
+
+  function loadPage() {
+    if (window.location.hash) {
+      APIController.genOauth();
+    }
+    if (localStorage.getItem("loggedIn")) {
+      APIController.myInfo();
+      APIController.mySongs();
+      document.querySelector('.infoBody').classList.add('hidden');
+      document.querySelector('.completedBody').classList.remove('hidden');
+    }
   }
 
   return {
@@ -69,7 +117,14 @@ var UIController = (() => {
     addMe: (info) => {
       addMyInfo(info);
     },
+    listSongs: (list) => {
+      listMySongs(list);
+    },
+    onLoad: () => {
+      loadPage();
+    },
   };
-})();
+})(APIController);
 
 $(".loginButton").on("click", UIController.loginPage);
+UIController.onLoad();
